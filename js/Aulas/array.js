a795438bdcc0e7505c3003d85ecd0b9adc949e65
Henrique Rodrigues Oliveira <henrique_krf@outlook.com>
2024-12-29 09:10:19 -0300

    Loops

--- js/Aulas/array.js
+++ js/Aulas/array.js
@@ -1,3 +1,19 @@
 var lista = ["Jose", "Henrique", "Rodrigues", 122];
 
+console.log(lista.indexOf("Henrique"));
+
+console.log(lista[1]);
+
+console.log((lista[1] = 500));
+console.log(lista[1]);
+
+lista.push("Zeca");
+
+lista.shift();
+
+lista.pop();
+
+console.log(lista.length);
+
+console.log(lista.join("; "));
 console.log(lista);
